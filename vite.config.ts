aad10c7c067adc87bb54007e1a612a68196b2473
Mikkel Oerum Skovgaard-Petersen <mikkeloerum@gmail.com>
2022-10-13 18:57:51 +0200

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyEdge from "@netlify/vite-plugin-netlify-edge";
 
 export default defineConfig(() => {
   return {
@@ -12,7 +11,6 @@ export default defineConfig(() => {
         ssr: { outDir: "netlify/edge-functions/entry.netlify-edge" },
       }),
       tsconfigPaths(),
-      netlifyEdge({ functionName: "entry.netlify-edge" }),
     ],
   };
 });
