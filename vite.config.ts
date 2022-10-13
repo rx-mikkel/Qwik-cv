2b7dc98156b8567266dd35bb5b09797d74ea4ab7
Mikkel Oerum Skovgaard-Petersen <mikkeloerum@gmail.com>
2022-10-13 18:59:44 +0200

    add back in netlify setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyEdge from "@netlify/vite-plugin-netlify-edge";
 
 export default defineConfig(() => {
   return {
@@ -11,6 +12,7 @@ export default defineConfig(() => {
         ssr: { outDir: "netlify/edge-functions/entry.netlify-edge" },
       }),
       tsconfigPaths(),
+      netlifyEdge({ functionName: "entry.netlify-edge" }),
     ],
   };
 });
